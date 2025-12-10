5ae9b56e6216a2a3af2855d8106b7820a20a4867
Abhinand Jha <jha.abhinand1@gmail.com>
2025-12-10 22:12:42 +0530

    Day1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,28 @@
 plugins {
     kotlin("jvm") version "2.2.21"
+    application
+}
+
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 sourceSets {
     main {
         kotlin.srcDir("src")
     }
+}
+
+application {
+    mainClass = "Day01Kt"
 }
 
 tasks {
--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,53 @@
+import kotlin.math.abs
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var start = 50
+        var ans = 0
+        val max = 100
+        for (line in input) {
+            val direction = line.first()
+            val steps = line.substring(1).toInt()
+            start = when (direction) {
+                'L' -> ((start - steps) + max) % max
+                'R' -> (start + steps) % max
+                else -> throw IllegalArgumentException("Invalid direction: $direction")
+            }
+            if (start == 0) ans++
+        }
+        return ans
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var start = 50
+        var ans = 0
+        val max = 100
+        for (line in input) {
+            val direction = line.first()
+            val steps = line.substring(1).toInt()
+            start = when (direction) {
+                'L' -> {
+                    var newStart = start
+                    for (i in 1..steps) {
+                        newStart = (newStart - 1 + max).mod(max)
+                        if (newStart == 0) ans++
+                    }
+                    newStart
+                }
+                'R' -> {
+                    var newStart = start
+                    for (i in 1..steps) {
+                        newStart = (newStart + 1).mod(max)
+                        if (newStart == 0) ans++
+                    }
+                    newStart
+                }
+                else -> throw IllegalArgumentException("Invalid direction: $direction")
+            }
+            // if (start == 0) ans++
+        }
+        return ans
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
