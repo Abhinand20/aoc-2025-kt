958833967f35b7a6f0c2c8161dd3358361b55d3f
Abhinand Jha <jha.abhinand1@gmail.com>
2025-12-11 23:00:10 +0530

    Day2p2 idiomatic kotlin

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,20 +1,46 @@
-import kotlin.math.abs
-import java.math.BigInteger
+fun Long.isSymmetric(): Boolean {
+    val numStr = this.toString()
+    val numDigits = numStr.length
+    if (numDigits % 2 != 0) return false
+    val half = numDigits/2
+    return numStr.substring(0, half) == numStr.substring(half)
+}
+
+/**
+ * (neat math trick) Checks if a number is periodic using the following method:
+ *
+ * 1. Concatenate the number with itself, making the repeating pattern continuous.
+ *    Example: 565656  →  565656565656
+ *
+ * 2. Search for the first occurrence of the original number within this concatenated string,
+ *    starting from position 1 (i.e., after the very first character).
+ *    Example: For "5656", in "56565656", "5656" appears again starting at index 2.
+ *
+ * 3. If this occurrence is found at an index less than the length of the number,
+ *    the number is periodic. The substring from index 0 up to that index is the repeating pattern.
+ */
+fun Long.isPeriodic(): Boolean {
+    val s = this.toString()
+    return (s + s).indexOf(s, 1) < s.length
+}
+
+
 fun main() {
+    // Returns a sequence of long ranges to save memory.
+    fun parseRanges(input: String): Sequence<LongRange> {
+        return input.splitToSequence(",").map {
+            rangeStr -> 
+                val (low, high) = rangeStr.split("-")
+                low.toLong()..high.toLong()
+            }
+    }
+
     fun part1(input: List<String>): Long {
-        fun isNumInvalid(numStr: String): Boolean {
-            // Determine if the number meets the condition
-            val numDigits = numStr.length
-            if (numDigits % 2 != 0) return false
-            return numStr.substring(0, numDigits/2) == numStr.substring(numDigits/2)
-        }
-        var allRanges = input[0].split(",")
+        var allRanges = parseRanges(input.first())
         var ans = 0L
         for (range in allRanges) {
-            var low = range.split("-")[0].toLong()
-            var high = range.split("-")[1].toLong()
-            for (num in low..high) {
-                if (isNumInvalid(num.toString())) {
+            for (num in range) {
+                if (num.isSymmetric()) {
                     ans += num
                 }
             }
@@ -36,13 +62,11 @@ fun main() {
             }
             return false
         }
-        var allRanges = input[0].split(",")
+        var allRanges = parseRanges(input.first())
         var ans = 0L
         for (range in allRanges) {
-            var low = range.split("-")[0].toLong()
-            var high = range.split("-")[1].toLong()
-            for (num in low..high) {
-                if (isNumInvalid(num.toString())) {
+            for (num in range) {
+                if (num.isPeriodic()) {
                     ans += num
                 }
             }
