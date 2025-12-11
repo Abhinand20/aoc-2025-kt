d55ad030ec5a5b742714b473eac9c3503689ea8e
Abhinand Jha <jha.abhinand1@gmail.com>
2025-12-11 17:00:01 +0530

    Day2p2

--- src/Day02.kt
+++ src/Day02.kt
@@ -22,11 +22,35 @@ fun main() {
         return ans
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        // Group the digits and find if substr starts with the same digits
+        fun isNumInvalid(numStr: String): Boolean {
+            for (i in 0..<numStr.length/2) {
+                var currDigit = numStr.substring(0,i+1)
+                // Match the whole group of digits till the end.
+                var pattern = Regex("^($currDigit)+$")
+                var remainingDigits = numStr.substring(i+1)
+                if (pattern.matches(remainingDigits)) {
+                    return true
+                }
+            }
+            return false
+        }
+        var allRanges = input[0].split(",")
+        var ans = 0L
+        for (range in allRanges) {
+            var low = range.split("-")[0].toLong()
+            var high = range.split("-")[1].toLong()
+            for (num in low..high) {
+                if (isNumInvalid(num.toString())) {
+                    ans += num
+                }
+            }
+        }
+        return ans
     }
 
     val input = readInput("Day02")
     part1(input).println()
-    // part2(input).println()
+    part2(input).println()
 }
