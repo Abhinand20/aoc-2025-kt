f14a38260bcde26cca665241d9e2e85fed6b3c61
Abhinand Jha <jha.abhinand1@gmail.com>
2025-12-15 23:31:12 +0530

    Day5p2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,4 +1,10 @@
-data class Range(val low: Long, val high: Long)
+data class Range(val low: Long, val high: Long) {
+    fun isOverlapping(other: Range): Boolean {
+        return this.low <= other.high && other.low <= this.high
+    }
+
+    fun numElements() = high - low + 1
+}
 
 fun List<Range>.isInRange(value: Long): Boolean {
     return this.map { range ->
@@ -6,8 +12,26 @@ fun List<Range>.isInRange(value: Long): Boolean {
     }.filter { it == true }.isNotEmpty()
 }
 
-fun main() {
 
+fun List<Range>.getMergedRanges(): List<Range> {
+    if (this.isEmpty()) return this
+    val ranges = this.sortedBy { it.low }
+    val mergedRanges = mutableListOf<Range>(ranges.first())
+    for (range in ranges.subList(1, ranges.size)) {
+        if (mergedRanges.last().isOverlapping(range)) {
+            // update the intervals
+            var updatedVal = Range(mergedRanges.last().low, maxOf(mergedRanges.last().high, range.high))
+            @Suppress("JAVA_MODULE_DOES_NOT_EXPORT_PACKAGE") // False positive
+            mergedRanges.removeLast()
+            mergedRanges.add(updatedVal)
+        } else {
+            mergedRanges.add(range)
+        }
+    }
+    return mergedRanges.toList()
+}
+
+fun main() {
     fun part1(input: List<String>): Int {
         val sepIdx = input.indexOf("")
         val allRanges = input.subList(0, sepIdx).map { curr ->
@@ -22,8 +46,15 @@ fun main() {
         }.count { it == true }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        val sepIdx = input.indexOf("")
+        val allRanges = input.subList(0, sepIdx).map { curr ->
+            val tokens = curr.split('-')
+            Range(tokens[0].toLong(), tokens[1].toLong())
+        }
+        val merged = allRanges.getMergedRanges()
+        println("Merged: $merged")
+        return merged.map { it.numElements() }.sum()
     }
 
     val input = readInput("Day05")
