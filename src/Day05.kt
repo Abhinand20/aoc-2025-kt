e30d9e9659d251643416ed9eecc876de853263bb
Abhinand Jha <jha.abhinand1@gmail.com>
2025-12-16 11:51:17 +0530

    Idiomatic kotlin using LongRange

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,58 +1,52 @@
-data class Range(val low: Long, val high: Long) {
-    fun isOverlapping(other: Range): Boolean {
-        return this.low <= other.high && other.low <= this.high
-    }
-    fun numElements() = high - low + 1
+fun LongRange.intersects(other: LongRange): Boolean {
+    return this.first <= other.last && this.last >= other.first
 }
 
-fun List<Range>.isInRange(value: Long): Boolean {
-    return this.map { range ->
-        value >= range.low && value <= range.high
-    }.filter { it == true }.isNotEmpty()
+fun LongRange.merge(other: LongRange): LongRange {
+    return minOf(this.first, other.first)..maxOf(this.last, other.last)
 }
 
-fun List<Range>.getMergedRanges(): List<Range> {
-    if (this.isEmpty()) return this
-    val ranges = this.sortedBy { it.low }
-    val mergedRanges = mutableListOf<Range>(ranges.first())
-    for (range in ranges.subList(1, ranges.size)) {
-        if (mergedRanges.last().isOverlapping(range)) {
-            // update the intervals
-            var updatedVal = Range(mergedRanges.last().low, maxOf(mergedRanges.last().high, range.high))
-            @Suppress("JAVA_MODULE_DOES_NOT_EXPORT_PACKAGE") // False positive
-            mergedRanges.removeLast()
-            mergedRanges.add(updatedVal)
-        } else {
-            mergedRanges.add(range)
+fun List<LongRange>.isInRange(value: Long): Boolean {
+    return this.any { value in it }
+}
+
+fun List<LongRange>.mergeOverlapping(): List<LongRange> {
+    if (isEmpty()) return this
+
+    return sortedBy { it.first }
+        .fold(mutableListOf()) { acc, range ->
+            val last = acc.lastOrNull()
+            if (last != null && last.intersects(range)) {
+                acc[acc.lastIndex] = last.merge(range)
+            } else {
+                acc.add(range)
+            }
+            acc
         }
-    }
-    return mergedRanges.toList()
 }
 
 fun main() {
     fun part1(input: List<String>): Int {
         val sepIdx = input.indexOf("")
-        val allRanges = input.subList(0, sepIdx).map { curr ->
-            val tokens = curr.split('-')
-            Range(tokens[0].toLong(), tokens[1].toLong())
+        val allRanges = input.take(sepIdx).map { curr ->
+            val (low, high) = curr.split('-').map { it.toLong() }
+            low..high
         }
-        val allIds = input.subList(sepIdx+1, input.size).map {
+        val allIds = input.drop(sepIdx+1).map {
             it.toLong()
         }
-        return allIds.map { currVal ->
-            allRanges.isInRange(currVal)
-        }.count { it == true }
+        return allIds.count { value -> 
+            allRanges.isInRange(value)
+        }
     }
 
     fun part2(input: List<String>): Long {
         val sepIdx = input.indexOf("")
-        val allRanges = input.subList(0, sepIdx).map { curr ->
-            val tokens = curr.split('-')
-            Range(tokens[0].toLong(), tokens[1].toLong())
+        val allRanges = input.take(sepIdx).map { curr ->
+            val (low, high)= curr.split('-').map { it.toLong() }
+            low..high
         }
-        val merged = allRanges.getMergedRanges()
-        println("Merged: $merged")
-        return merged.map { it.numElements() }.sum()
+        return allRanges.mergeOverlapping().sumOf { it.last - it.first + 1}
     }
 
     val input = readInput("Day05")
